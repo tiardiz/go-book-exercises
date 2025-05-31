5bc091774ce227c7fea08c10939c5fb5ca7612c5
Orachimaru <ardaktleules11@gmail.com>
2025-05-31 16:49:57 +0500

    add metod dup 3

---
+++ dups.1-3/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+// ---------
+// DUP - 1
+// ---------
+
+// func main() {
+// 	counts := make(map[string]int)
+// 	input := bufio.NewScanner(os.Stdin)
+// 	for input.Scan() {
+// 		counts[input.Text()]++
+// 	}
+
+// 	for line, n := range counts {
+// 		if n > 1 {
+// 			fmt.Printf("%d\t%s\n", n, line)
+// 		}
+// 	}
+// }
+
+// ---------
+// DUP - 2
+// ---------
+
+// func main() {
+// 	counts := make(map[string]int)
+// 	files := os.Args[1:]
+// 	if len(files) == 0 {
+// 		countLines(os.Stdin, counts)
+// 	} else {
+// 		for _, arg := range files {
+// 			f, err := os.Open(arg)
+// 			if err != nil {
+// 				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+// 				continue
+// 			}
+// 			countLines(f, counts)
+// 			f.Close()
+// 		}
+// 	}
+// 	for line, n := range counts {
+// 		if n > 1 {
+// 			fmt.Printf("%d\t%s\n", n, line)
+// 		}
+// 	}
+// }
+
+// func countLines(f *os.File, counts map[string]int) {
+// 	input := bufio.NewScanner(f)
+// 	for input.Scan() {
+// 		counts[input.Text()]++
+// 	}
+// }
+
+// ---------
+// DUP - 3
+// ---------
+
+func main() {
+	counts := make(map[string]int)
+	for _, filename := range os.Args[1:] {
+		data, err := os.ReadFile(filename)
+		if err != nil {
+			fmt.Printf("error in %v", filename)
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
+		}
+
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%v: %d\n", line, n)
+		}
+	}
+}
